dcd4889f7a005ba8504d09e981b045135a1fc2f6
Walker <ryan.cjw@gmail.com>
2017-09-25 21:26:22 -0700

    DATASTUFF

--- Datasets/post.m
+++ Datasets/post.m
@@ -1,9 +1,20 @@
+%Todo
+%-------------------------------------------------------------
+% 1. Implement Filename into plot titles
+%-------------------------------------------------------------
 pkg load signal;
 
-clc; clear; close all;
+clc; clear;
 
 FontS = 20;
 
+%File location
+%-------------------------------------------------------------
+filename = 'Sept/Sept19-25.csv';
+M = csvread(filename);
+
+%Defining placements
+%-------------------------------------------------------------
 BTCticker = 3;
 BTCvol = 4;
 BTCsen = 5;
@@ -14,68 +25,132 @@ LTCvol = 8;
 LTCsen = 9;
 LTCcost = 10;
 
-%filename = 'Sept17-18.csv';
-%filename = 'Sept17_2017.csv';
-filename = 'Sept19-24.csv';
-M = csvread(filename);
-
 Cost = M(1:end, BTCcost);
 Sen = M(1:end, BTCsen);
 Vol = M(1:end, BTCvol);
 
-titleRaw = 'Cost of Bitcoin vs. Bitcoin Sentiment';
-titleDer = 'Cost of Bitcoin vs. (d/dt)Bitcoin Sentiment';
-
 %Cost = M(1:end, LTCcost);
 %Sen = M(1:end, LTCsen);
 
-[b,a]=butter(3, 0.01);
-filteredSen = filter(b,a,Sen);
-filteredCost = sgolayfilt(Cost);
-filteredVol = filter(b,a,Vol);
-
-normSen = filteredSen .* (1 / max(filteredSen)); %Normalize
-normVol = filteredVol .* (1 / max(filteredVol)); %Normalize
-normCost = Cost .* (1 / max(Cost)); %Normalize
+titleRaw = 'Cost of Bitcoin vs. Bitcoin Sentiment';
+titleDer = 'Cost of Bitcoin vs. (d/dt)Bitcoin Sentiment';
 
 x = 0:length(M) - 1;
 x2 = 0:length(M) - 1;
 
-len = length(Sen) - 100; %Want to analyse half the dataset
+%Filtering
+%-------------------------------------------------------------
+windowSize = 50; 
+bb = (1/windowSize)*ones(1,windowSize);
+aa = 1;
 
-for i = 1:len
-	Cost(1) = [];
-	Sen(end) = [];
-	meanResult(i) = mean(Cost .*Sen);
-end
+[b,a]=butter(3, 0.01);
+filteredSen = filter(b,a,Sen);
+filteredCost = filter(bb,aa,Cost);
+filteredVol = filter(b,a,Vol);
 
-ax = plotyy(x, Cost, x2, filteredSen);
+%For some reason the average filter fucks up the first little bit
+Cost(1:windowSize) = [];
+filteredSen(1:windowSize) = [];
+filteredCost(1:windowSize) = [];
+filteredVol(1:windowSize) = [];
+x(1:windowSize) = [];
+x2(1:windowSize) = [];
+
+%Normalise
+%-------------------------------------------------------------
+normSen = filteredSen .* (1 / max(filteredSen)); %Normalize
+normVol = filteredVol .* (1 / max(filteredVol)); %Normalize
+normCost = Cost .* (1 / max(Cost)); %Normalize
+
+%Initial Plotting (Raw)
+%-------------------------------------------------------------
+figure(1)
+ax = plotyy(x, filteredCost, x2, filteredSen);
 title(titleRaw, 'FontSize', FontS);
 ylabel(ax(1), 'Cost (USD)', 'FontSize', FontS);
 ylabel(ax(2), 'Crypto Sentiment', 'FontSize', FontS);
-legend('Cost', 'Sentiment', 'Sentiment Volume')
+legend('Cost', 'Sentiment')
+
+figure(2)
+x3 = x;
+x3(end) = [];
+ax = plotyy(x3, diff(filteredCost), x3, diff(filteredSen));
+title(titleRaw, 'FontSize', FontS);
+ylabel(ax(1), 'Cost (USD)', 'FontSize', FontS);
+ylabel(ax(2), 'Crypto Sentiment', 'FontSize', FontS);
+legend('Cost', 'Sentiment')
+
+%break;
+
+%Find time lag
+%-------------------------------------------------------------
+len = length(Sen) / 4 ; %Want to analyse quarter the dataset
+
+TempCost = Cost;
+TempSen = filteredSen;
+
+for i = 1:len
+	TempCost(1) = [];
+	TempSen(end) = [];
+	meanResult(i) = mean(TempCost - TempSen);
+end
+
+figure(3)
+plot(meanResult);
+
+title('Mean result time shifted correlation of Cost & Sentiment', 'FontSize', FontS);
+xlabel('Time shift', 'FontSize', FontS);
+ylabel('Mean value (Lower is better)', 'FontSize', FontS);
+
+%Find time lag ds/dt
+%-------------------------------------------------------------
+lenDer = length(Sen) / 4 ; %Want to analyse quarter the dataset
+
+TempCost = diff(filteredCost);
+TempSen = diff(filteredSen);
+
+for i = 1:len
+	TempCost(1) = [];
+	TempSen(end) = [];
+	meanResultdiff(i) = mean(abs(TempCost - TempSen));
+end
+
+figure(4)
+plot(meanResultdiff);
+title('Mean result time shifted correlation of dCost/dt and dSen/dt', 'FontSize', FontS);
+xlabel('Time shift', 'FontSize', FontS);
+ylabel('Mean value (Lower is better)', 'FontSize', FontS);
+
+
+%Apply Lag
+%-------------------------------------------------------------
+lag = find(meanResult == min(meanResult));
+x2 = x2 + lag;
 
 break;
 
+figure(4)
+ax2 = plotyy(x, filteredCost, x2, filteredSen);
+legend('Cost', 'Sentiment (timeshifted)')
 
-%Time shift
-x2 = x2 + 800;
+break;
 
+figure(5)
 ax = plotyy(x, filteredCost, x2, [normSen normVol]);
 title(titleRaw, 'FontSize', FontS);
 ylabel(ax(1), 'Cost (USD)', 'FontSize', FontS);
 ylabel(ax(2), 'Crypto Sentiment', 'FontSize', FontS);
 legend('Cost', 'Sentiment(Normalised)', 'Sentiment Volume(Normalised)')
 
-
-figure();
+figure(6);
 dsdt = diff(filteredSen) ./ 10;
 x2(end) = [];
-ax2 = plotyy(x, Cost, x2, dsdt);
+ax2 = plotyy(x, filteredCost, x2, dsdt);
 title(titleDer, 'FontSize', FontS);
 ylabel(ax2(1), 'Cost (USD)', 'FontSize', FontS);
 ylabel(ax2(2), 'Crypto Sentiment', 'FontSize', FontS);
 
 %figure();
 %plotyy(x, Cost, x, normVol-normSen)
-
+break;
