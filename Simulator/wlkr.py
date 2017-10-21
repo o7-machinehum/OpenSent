586c2587aa172eda1f7d925f8d9145acce351f26
Walker <ryan.cjw@gmail.com>
2017-10-20 22:19:19 -0700

    Finished of my bot, there are some problems with the holes in the dataset... I don't know how to fix this

--- Simulator/wlkr.py
+++ Simulator/wlkr.py
@@ -10,13 +10,18 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
+windowSize = 50;
+def runningMeanFast(x, N):
+	return np.convolve(x, np.ones((N,))/N)[(N-1):]
+
 file = "../Datasets/Oct"
 BTC = 0 #Index value
 
 sim = Market(file, 1000)
 
 #We need an initial amount of data to calculate the lag
-RequiredLag = 10*2*60 #10hrs * 2 * 60 to get to elements
+RequiredLag = 50*2*60 #10hrs * 2 * 60 to get to elements
+MaxShiftLen = 10*2*60
 
 M = np.arange(2*RequiredLag, dtype = np.float).reshape(RequiredLag, 2)
 
@@ -30,14 +35,35 @@ Value = M[1:len(M), 0]
 Sen = M[1:len(M), 1]
 
 #TODO
-#1. Fitler both, sentiment is butter, cost should be moving average
+#1. Fitler both, sentiment is butter, cost should be moving average - done
 b, a = butter(3, 0.1, btype='low')
 Sen = lfilter(b, a, Sen)
 
-plt.plot(Sen)
-plt.show()
+Value = runningMeanFast(Value, windowSize)
+Value = Value[0 : len(Value) - windowSize] #The last elements get all fucked up
+Sen = Sen[0 : len(Sen) - windowSize]
 
-#2. Diff
-#3. Time Corr Vector
+#3. Take norms
+nSen = Sen - min(Sen);
+nSen = nSen*(1/max(nSen));
+nValue = Value - min(Value);
+nValue = nValue*(1/max(nValue));
+
+#3. Diff
+dSen = np.diff(nSen);
+dValue = np.diff(nValue);
+
+#4. Time Corr Vector
+tValue = dValue;
+tSen = dSen;
+
+meanResult = np.zeros(MaxShiftLen)
+
+for i in range(0,MaxShiftLen) :
+	tValue = tValue[1:len(tValue)];
+	tSen = tSen[0:len(tSen) - 1];
+	meanResult[i] = np.mean(tValue - tSen);
+
 #4. Find lag
-
+plt.plot(meanResult)
+plt.show()
