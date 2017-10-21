dabdcc6eecb7afff4ffff3c3c9164e0788ae669a
Walker <machinehum@walkerlovesyou>
2017-10-20 21:30:41 -0700

    Working on walkers bot

--- Simulator/wlkr.py
+++ Simulator/wlkr.py
@@ -1,15 +1,43 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 -i
 #wlkr's bot
 
 from Market import Market
 from random import randint
+from scipy import signal
+from scipy.signal import butter, lfilter, freqz
+
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
 
 file = "../Datasets/Oct"
-sim = Market(file, 1000)
-#sim.deposit_USD(1000); #Lets start with 1k
+BTC = 0 #Index value
 
-for i in range(0, 10):
-	print("time ", sim.get_time())
-	print("USD: ", sim.get_USD())
-	print("BTC: ", sim.get_CC(0))
-	print("BTV val: ", sim.get_CC_value(0))
+sim = Market(file, 1000)
+
+#We need an initial amount of data to calculate the lag
+RequiredLag = 10*2*60 #10hrs * 2 * 60 to get to elements
+
+M = np.arange(2*RequiredLag, dtype = np.float).reshape(RequiredLag, 2)
+
+#Initial lag finder
+for i in range(0, RequiredLag):
+	M[i,0] = sim.get_CC_value(BTC)
+	M[i,1] = sim.get_sentiment(BTC)
+	sim.inc_time(30)
+
+Value = M[1:len(M), 0]
+Sen = M[1:len(M), 1]
+
+#TODO
+#1. Fitler both, sentiment is butter, cost should be moving average
+b, a = butter(3, 0.1, btype='low')
+Sen = lfilter(b, a, Sen)
+
+plt.plot(Sen)
+plt.show()
+
+#2. Diff
+#3. Time Corr Vector
+#4. Find lag
+
