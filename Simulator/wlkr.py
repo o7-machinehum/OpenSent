04b2c10918d84f27bd75a96a2b288f060007e276
Walker <ryan.cjw@gmail.com>
2017-10-23 20:54:48 -0700

    Worked on wlkrBot

--- Simulator/wlkr.py
+++ Simulator/wlkr.py
@@ -22,13 +22,16 @@ BTC = 0 #Index value
 sim = Market(file, 1000)
 
 #We need an initial amount of data to calculate the lag
-RequiredLag = 10*2*60 #10hrs * 2 * 60 to get to elements
-MaxShiftLen = 5*2*60
+tau = 10*2*60 #10hrs * 2 * 60 to get to elements
+tauD = 5*2*60
 
-M = np.arange(2*RequiredLag, dtype = np.float).reshape(RequiredLag, 2)
+#Thresholding - this is used when finding buy/sell triggers
+Thresh = 0.8 
+
+M = np.arange(2*tau, dtype = np.float).reshape(tau, 2)
 
 #Initial lag finder
-for i in range(0, RequiredLag):
+for i in range(0, tau):
 	M[i,0] = sim.get_CC_value(BTC)
 	M[i,1] = sim.get_sentiment(BTC)
 	sim.inc_time(30)
@@ -45,6 +48,8 @@ Value = runningMeanFast(Value, windowSize)
 Value = Value[0 : len(Value) - windowSize] #The last elements get all fucked up
 Sen = Sen[0 : len(Sen) - windowSize]
 
+#Everything above here is basically init code. Below should now loop.
+#--------------------------------------------------------------------
 #3. Take norms
 nSen = Sen - min(Sen);
 nSen = nSen*(1/max(nSen));
@@ -59,17 +64,46 @@ dValue = np.diff(nValue);
 tValue = dValue;
 tSen = dSen;
 
-meanResult = np.zeros(MaxShiftLen)
+meanResult = np.zeros(tauD)
 
-for i in range(0,MaxShiftLen) :
+for i in range(0,tauD) :
 	tValue = tValue[1:len(tValue)];
 	tSen = tSen[0:len(tSen) - 1];
 	meanResult[i] = np.mean(tValue - tSen);
 
 #4. Find lag
-#plt.plot(meanResult)
-plt.plot(dSen)
-plt.show()
+#plt.plot(dSen)
+tauL = meanResult.argmin()
+SenOP = dSen[tau-tauL:tau]
+
+#5. Find the buy and sell triggers
+BuyTrigger = np.array(1)
+SellTrigger = np.array(1)
+
+for i in range(0,len(SenOP)):
+	if SenOP[i] > max(SenOP) * Thresh:
+		BuyTrigger = np.append(BuyTrigger, i)
+	if SenOP[i] < min(SenOP) * Thresh:
+		SellTrigger = np.append(SellTrigger,  i)
+
+for i in range(0, tau):
+	if i in BuyTrigger:
+		print('Buying')
+		sim.buy_CC(BTC, 10)
+	if i in SellTrigger:
+		print('Selling')
+		sim.sell_CC(BTC, 10)
+	
+	#M[i,0] = sim.get_CC_value(BTC)
+	#M[i,1] = sim.get_sentiment(BTC)
+	sim.inc_time(30)
+
+#Value = M[1:len(M), 0]
+#Sen = M[1:len(M), 1]
+
+#Alright we've gone through one full tauL
+sim.sell_CC(BTC, sim.get_CC(BTC))
+sim.plot_trading_stats(BTC)
 
 #-----------------------Part Two (This is where the magic happens)------------------
 
