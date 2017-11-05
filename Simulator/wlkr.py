2486b0abff6c37864d48a470694656e44e8aebd7
Walker <ryan.cjw@gmail.com>
2017-11-04 18:34:18 -0700

    Simulator working fucking awesomely

--- Simulator/wlkr.py
+++ Simulator/wlkr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3 -i
 #wlkr's bot
 
+#pdb.set_trace() <- for breaks
+
 from Market import Market
 from random import randint
 from scipy import signal
@@ -9,14 +11,21 @@ from scipy.signal import butter, lfilter, freqz
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+import pdb
 
 windowSize = 50;
 def runningMeanFast(x, N):
 	return np.convolve(x, np.ones((N,))/N)[(N-1):]
 
-file = "../Datasets/Oct"
-#file = "../Datasets/Temp" 
+#Trading methods
+#--------------------------------------------------------------------
+#Method = 'SimpleSam'
+Method = 'TycoonJoe'
 
+file = "../Datasets/Oct"
+#file = "../Datasets/Contig" 
+
+DebugBreaks = True
 Debug = False
 Plot = False 
 
@@ -31,7 +40,7 @@ tau = 10*hrToSamples #10hrs * 2 * 60 to get to elements
 tauD = 5*hrToSamples #How much shift are we allowing for time lag
 
 #Thresholding - this is used when finding buy/sell triggers
-Thresh = 0.6 
+Thresh = 0.9 
 
 M = np.arange(2*tau, dtype = np.float).reshape(tau, 2)
 time = [None] * tau
@@ -44,12 +53,11 @@ for i in range(0, tau):
 	M[i,0] = sim.get_CC_value(BTC)
 	M[i,1] = sim.get_sentiment(BTC)
 	time[i] = sim.get_time()
-	print(i);
-	
+	sim.inc_time(30)
 
 #Everything above here is basically init code. Below should now loop.
 
-for k in range(0,100):
+for k in range(0,75):
 #1. Fitler both, sentiment is butter, cost should be moving average - done
 #--------------------------------------------------------------------
 	Value = M[1:len(M), 0]
@@ -62,13 +70,13 @@ for k in range(0,100):
 	Value = Value[0 : len(Value) - windowSize] #The last elements get all fucked up
 	Sen = Sen[0 : len(Sen) - windowSize]
 
-#3. Take norms
+#2. Take norms
 #--------------------------------------------------------------------
 	nSen = Sen - min(Sen);
 	nSen = nSen*(1/max(nSen));
 	nValue = Value - min(Value);
 	nValue = nValue*(1/max(nValue));
-
+	
 #3. Diff
 #--------------------------------------------------------------------
 	dSen = np.diff(nSen);
@@ -107,7 +115,6 @@ for k in range(0,100):
 		plt.show()
 
 #5. Find the buy and sell triggers
-	NextBuy = True
 #--------------------------------------------------------------------
 	BuyTrigger = np.array(1)
 	SellTrigger = np.array(1)
@@ -123,17 +130,19 @@ for k in range(0,100):
 		print('Previous Buy Triggers > ', BuyTrigger)
 		print('Previous Sell Triggers > ', SellTrigger)
 
+	LastSenOPbuy = 0
+	LastSenOPsell = 0
+
 	for i in range(0,len(SenOP)):
-		#if SenOP[i] > max(SenOP) * Thresh:
-		if SenOP[i] > BuySen:
-			#if NextBuy == True:
-			BuyTrigger = np.append(BuyTrigger, i)
-			NextBuy = False
-		#if SenOP[i] < min(SenOP) * Thresh:
-		if SenOP[i] < SellSen:
-			#if NextBuy == False:
-			SellTrigger = np.append(SellTrigger,  i)
-			NextBuy = True
+		if SenOP[i] > max(SenOP) * Thresh:
+			if i > LastSenOPbuy + 1*hrToSamples:
+				BuyTrigger = np.append(BuyTrigger, i)
+				LastSenOPbuy = i #To avoid continious buying
+
+		if SenOP[i] < min(SenOP) * Thresh:
+			if i > LastSenOPsell + 1*hrToSamples:
+				SellTrigger = np.append(SellTrigger,  i)
+				LastSenOPsell = i
 	
 	if Debug:
 		print('Buy Triggers > ', BuyTrigger)
@@ -143,29 +152,50 @@ for k in range(0,100):
 		#plt.show();
 
 #6. Lets start to trade
-#TODO: If the value you're selling is lower, don't sell (Maybe)
-#TODO: Make sure the plotting is working properly - Print the times of selling and buying
 #--------------------------------------------------------------------
-	for i in range(0, tau):
-		if i in BuyTrigger:
-			if Debug:
-				print('Buying at: ', sim.get_time())
-				print(sim.buy_CC(BTC, 10))
-			else:
-				sim.buy_CC(BTC, 10);
-		if i in SellTrigger:
-			if Debug:
-				print('Selling at: ', sim.get_time())
-				print(sim.sell_CC(BTC, (10 / sim.get_CC_value(BTC))))
-			else:
-				sim.sell_CC(BTC, (10 / sim.get_CC_value(BTC)))
+
+#Method one - Simple Sam. Buy on buy triggers and sell on sell triggers
+#--------------------------------------------------------------------
+	if Method == 'SimpleSam':
+		NextBuy = True
+		
+		for i in range(0, tau):
+			if i in BuyTrigger:
+				if NextBuy == True:
+					sim.buy_CC(BTC, 10);
+					NextBuy = False
+			if i in SellTrigger:	
+				if NextBuy == False:
+					sim.sell_CC(BTC, (10 / sim.get_CC_value(BTC)))	
+					NextBuy = True
+
+			M[i,0] = sim.get_CC_value(BTC)
+			M[i,1] = sim.get_sentiment(BTC)
+			sim.inc_time(30)
+
+#Method Two - Buy on buy triggers and sell when gains higher %10
+#						- If high density of sell triggers sell
+#--------------------------------------------------------------------
+	if Method == 'TycoonJoe':
+		buyPoint = 1000000.123412 #blaaa
+		Lastsell = 0
+		for i in range(0, tau):
+			if i in BuyTrigger:
+				sim.buy_CC(BTC, 0.5*sim.get_USD())
+				buyPoint = sim.get_CC_value(BTC)
 			
-		M[i,0] = sim.get_CC_value(BTC)
-		M[i,1] = sim.get_sentiment(BTC)
-		sim.inc_time(30)
+			if sim.get_CC_value(BTC) > (buyPoint + buyPoint*0.01):
+				if i > Lastsell + 1*hrToSamples:
+					sim.sell_CC(BTC, (0.5*sim.get_USD() / sim.get_CC_value(BTC)))
+					Lastsell = i
+
+			M[i,0] = sim.get_CC_value(BTC)
+			M[i,1] = sim.get_sentiment(BTC)
+			sim.inc_time(30)
+
+	#pdb.set_trace()
 
 #Alright we've gone through one full tauL
 sim.sell_CC(BTC, sim.get_CC(BTC))
 print(sim.get_USD())
 sim.plot_trading_stats(BTC)
-
